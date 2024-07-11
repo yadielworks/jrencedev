58d590e0180f0fb9b611387ac6f342c5085339c2
yadielworks <yadielvieyero@gmail.com>
2024-07-11 17:01:15 +0800

    Create tempmail.js

---
+++ tempmail.js
@@ -0,0 +1,1 @@
+{"action":{"getMail":{"&count=":"amount of email you want to generate","example":"/tempmail?action=getMail&count=5"},"getMessages":{"&email=":"Mail name you want to see inbox messages","example":"/tempmail?action=getMessages&email=EXAMPLE@gmail.com"}},"action=":"getMessages, getMail","error_msg":"Invalid action parameter"}
